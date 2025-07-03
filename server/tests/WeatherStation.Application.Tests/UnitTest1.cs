fc3bef3c83aeb8d969063eddc75cb009cad37335
MKKL <Mkkl3301@gmail.com>
2025-07-03 18:08:03 +0200

    feat: add projects for unit tests

---
+++ server/tests/WeatherStation.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherStation.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ server/tests/WeatherStation.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherStation.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
