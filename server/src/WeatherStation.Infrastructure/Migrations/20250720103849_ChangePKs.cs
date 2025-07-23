3ab6d0032912688a98a192321bd8fccee4a22569
MKKL <Mkkl3301@gmail.com>
2025-07-23 20:37:14 +0200

    add missing migrations from #10 feature/get-devices-of-user

---
+++ server/src/WeatherStation.Infrastructure/Migrations/20250720103849_ChangePKs.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherStation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePKs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Devices_Name",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Devices");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Devices",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_Id",
+                table: "Devices",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Devices_Id",
+                table: "Devices");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Devices",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Devices",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_Name",
+                table: "Devices",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
