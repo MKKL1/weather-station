fa437502bfc1062d95bb5d5f66bc106f47f07ba2
MKKL <Mkkl3301@gmail.com>
2026-02-18 23:06:06 +0100

    deploy: add endpoints from provisioning service

--- deploy/apim.tf
+++ deploy/apim.tf
@@ -48,20 +48,48 @@ resource "azurerm_api_management_named_value" "access_token_issuer_url" {
   secret              = false
 }
 
+resource "azurerm_api_management_named_value" "tenant_id" {
+  name                = "tenant-id"
+  resource_group_name = azurerm_resource_group.project_scope.name
+  api_management_name = azurerm_api_management.api_gateway.name
+  display_name        = "tenant-id"
+  value               = data.azurerm_client_config.current.tenant_id
+  secret              = false
+}
+
+resource "azurerm_api_management_named_value" "external_worker_client_id" {
+  name                = "external-worker-client-id"
+  resource_group_name = azurerm_resource_group.project_scope.name
+  api_management_name = azurerm_api_management.api_gateway.name
+  display_name        = "external-worker-client-id"
+  value               = azuread_application.external_worker_app.client_id
+  secret              = false
+}
+
+resource "azurerm_api_management_policy_fragment" "provisioning_jwt" {
+  api_management_id = azurerm_api_management.api_gateway.id
+  name              = "provisioning-jwt"
+  format            = "rawxml"
+  value             = file("${path.module}/policies/fragments/provisioning_jwt.xml")
+
+  depends_on = [
+    azurerm_api_management_named_value.access_token_issuer_url,
+    azurerm_api_management_named_value.function_app_client_id
+  ]
+}
+
+
+
+// ===============================================
+// Provisioning
+// ===============================================
+
 resource "azurerm_api_management_backend" "auth_backend" {
   name                = "auth-backend"
   resource_group_name = azurerm_resource_group.project_scope.name
   api_management_name = azurerm_api_management.api_gateway.name
   protocol            = "http"
   url                 = "https://${azurerm_function_app_flex_consumption.provisioning_function.default_hostname}/api/v1"
-}
-
-resource "azurerm_api_management_backend" "telemetry_backend" {
-  name                = "telemetry-backend"
-  resource_group_name = azurerm_resource_group.project_scope.name
-  api_management_name = azurerm_api_management.api_gateway.name
-  protocol            = "http"
-  url                 = "https://${azurerm_function_app_flex_consumption.main_function.default_hostname}/api/v1"
 }
 
 resource "azurerm_api_management_api" "provisioning_api" {
@@ -77,6 +105,8 @@ resource "azurerm_api_management_api" "provisioning_api" {
   service_url = "https://${azurerm_function_app_flex_consumption.provisioning_function.default_hostname}"
 }
 
+// /provisioning/{deviceId}/register
+
 resource "azurerm_api_management_api_operation" "device_register" {
   operation_id        = "device-register"
   api_name            = azurerm_api_management_api.provisioning_api.name
@@ -84,7 +114,13 @@ resource "azurerm_api_management_api_operation" "device_register" {
   resource_group_name = azurerm_resource_group.project_scope.name
   display_name        = "Register device"
   method              = "POST"
-  url_template        = "/register"
+  url_template        = "/{deviceId}/register"
+
+  template_parameter {
+    name     = "deviceId"
+    required = true
+    type     = "string"
+  }
 }
 
 resource "azurerm_api_management_api_operation_policy" "device_register_policy" {
@@ -94,8 +130,103 @@ resource "azurerm_api_management_api_operation_policy" "device_register_policy" 
   operation_id        = azurerm_api_management_api_operation.device_register.operation_id
 
   xml_content = file("${path.module}/policies/device_register.xml")
-  depends_on  = [azurerm_api_management_named_value.access_token_issuer_url, null_resource.swa_deploy]
+  depends_on  = [azurerm_api_management_policy_fragment.provisioning_jwt, null_resource.swa_deploy]
 }
+
+// /provisioning/{deviceId}/token
+
+resource "azurerm_api_management_api_operation" "token_refresh" {
+  operation_id        = "token-refresh"
+  api_name            = azurerm_api_management_api.provisioning_api.name
+  api_management_name = azurerm_api_management.api_gateway.name
+  resource_group_name = azurerm_resource_group.project_scope.name
+  display_name        = "Token Refresh"
+  method              = "POST"
+  url_template        = "/{deviceId}/token"
+
+  template_parameter {
+    name     = "deviceId"
+    required = true
+    type     = "string"
+  }
+}
+
+resource "azurerm_api_management_api_operation_policy" "token_refresh_policy" {
+  api_name            = azurerm_api_management_api.provisioning_api.name
+  api_management_name = azurerm_api_management.api_gateway.name
+  resource_group_name = azurerm_resource_group.project_scope.name
+  operation_id        = azurerm_api_management_api_operation.token_refresh.operation_id
+
+  xml_content = file("${path.module}/policies/token_refresh.xml")
+  depends_on  = [azurerm_api_management_policy_fragment.provisioning_jwt, null_resource.swa_deploy]
+}
+
+// /provisioning/{deviceId}/claim-code
+
+resource "azurerm_api_management_api_operation" "claim_code" {
+  operation_id        = "claim-code"
+  api_name            = azurerm_api_management_api.provisioning_api.name
+  api_management_name = azurerm_api_management.api_gateway.name
+  resource_group_name = azurerm_resource_group.project_scope.name
+  display_name        = "Generate claim code"
+  method              = "POST"
+  url_template        = "/{deviceId}/claim-code"
+
+  template_parameter {
+    name     = "deviceId"
+    required = true
+    type     = "string"
+  }
+}
+
+resource "azurerm_api_management_api_operation_policy" "claim_code_policy" {
+  api_name            = azurerm_api_management_api.provisioning_api.name
+  api_management_name = azurerm_api_management.api_gateway.name
+  resource_group_name = azurerm_resource_group.project_scope.name
+  operation_id        = azurerm_api_management_api_operation.claim_code.operation_id
+
+  xml_content = file("${path.module}/policies/claim_code.xml")
+  depends_on  = [azurerm_api_management_policy_fragment.provisioning_jwt, null_resource.swa_deploy]
+}
+
+// /provisioning/{deviceId}/claim
+
+resource "azurerm_api_management_api_operation" "device_claim" {
+  operation_id        = "device-claim"
+  api_name            = azurerm_api_management_api.provisioning_api.name
+  api_management_name = azurerm_api_management.api_gateway.name
+  resource_group_name = azurerm_resource_group.project_scope.name
+  display_name        = "Claim device"
+  method              = "POST"
+  url_template        = "/{deviceId}/claim"
+
+  template_parameter {
+    name     = "deviceId"
+    required = true
+    type     = "string"
+  }
+}
+
+resource "azurerm_api_management_api_operation_policy" "device_claim_policy" {
+  api_name            = azurerm_api_management_api.provisioning_api.name
+  api_management_name = azurerm_api_management.api_gateway.name
+  resource_group_name = azurerm_resource_group.project_scope.name
+  operation_id        = azurerm_api_management_api_operation.device_claim.operation_id
+
+  xml_content = file("${path.module}/policies/ext_worker_claim.xml")
+
+  depends_on = [
+    azurerm_api_management_named_value.tenant_id,
+    azurerm_api_management_named_value.external_worker_client_id,
+    azurerm_api_management_named_value.function_app_client_id
+  ]
+}
+
+
+
+// ===============================================
+// Telemetry
+// ===============================================
 
 resource "azurerm_api_management_api" "device_api" {
   name                  = "device-api"
@@ -108,25 +239,15 @@ resource "azurerm_api_management_api" "device_api" {
   subscription_required = false
 }
 
-resource "azurerm_api_management_api_operation" "token_refresh" {
-  operation_id        = "token-refresh"
-  api_name            = azurerm_api_management_api.device_api.name
-  api_management_name = azurerm_api_management.api_gateway.name
+resource "azurerm_api_management_backend" "telemetry_backend" {
+  name                = "telemetry-backend"
   resource_group_name = azurerm_resource_group.project_scope.name
-  display_name        = "Token Refresh"
-  method              = "POST"
-  url_template        = "/auth/token"
+  api_management_name = azurerm_api_management.api_gateway.name
+  protocol            = "http"
+  url                 = "https://${azurerm_function_app_flex_consumption.main_function.default_hostname}/api/v1"
 }
 
-resource "azurerm_api_management_api_operation_policy" "token_refresh_policy" {
-  api_name            = azurerm_api_management_api.device_api.name
-  api_management_name = azurerm_api_management.api_gateway.name
-  resource_group_name = azurerm_resource_group.project_scope.name
-  operation_id        = azurerm_api_management_api_operation.token_refresh.operation_id
-
-  xml_content = file("${path.module}/policies/token_refresh.xml")
-  depends_on  = [azurerm_api_management_named_value.access_token_issuer_url, null_resource.swa_deploy]
-}
+// /device/telemetry
 
 resource "azurerm_api_management_api_operation" "telemetry" {
   operation_id        = "telemetry"
@@ -148,30 +269,30 @@ resource "azurerm_api_management_api_operation_policy" "telemetry_policy" {
   depends_on  = [azurerm_api_management_named_value.access_token_issuer_url, null_resource.swa_deploy]
 }
 
-resource "azurerm_api_management_diagnostic" "global_diagnostics" {
-  identifier               = "applicationinsights"
-  resource_group_name      = azurerm_resource_group.project_scope.name
-  api_management_name      = azurerm_api_management.api_gateway.name
-  api_management_logger_id = azurerm_api_management_logger.apim_logger.id
+# resource "azurerm_api_management_diagnostic" "global_diagnostics" {
+#   identifier               = "applicationinsights"
+#   resource_group_name      = azurerm_resource_group.project_scope.name
+#   api_management_name      = azurerm_api_management.api_gateway.name
+#   api_management_logger_id = azurerm_api_management_logger.apim_logger.id
 
-  verbosity = var.environment == "prod" ? "information" : "verbose"
-  http_correlation_protocol = "W3C"
-  always_log_errors = true
-  log_client_ip     = true
-  sampling_percentage = var.environment == "prod" ? 5.0 : 100.0
+#   verbosity                 = var.environment == "prod" ? "information" : "verbose"
+#   http_correlation_protocol = "W3C"
+#   always_log_errors         = true
+#   log_client_ip             = true
+#   sampling_percentage       = var.environment == "prod" ? 5.0 : 100.0
 
-  frontend_request {
-    body_bytes = 0
-    headers_to_log = ["User-Agent"]
-  }
+#   frontend_request {
+#     body_bytes     = 0
+#     headers_to_log = ["User-Agent"]
+#   }
 
-  backend_request {
-    body_bytes = 0
-    headers_to_log = ["traceparent"] 
-  }
+#   backend_request {
+#     body_bytes     = 0
+#     headers_to_log = ["traceparent"]
+#   }
 
-  backend_response {
-    body_bytes = 0
-    headers_to_log = []
-  }
-}
+#   backend_response {
+#     body_bytes     = 0
+#     headers_to_log = []
+#   }
+# }
