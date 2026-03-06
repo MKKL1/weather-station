a20f5e19fbaa66f1c82b3992240f3c937206b200
MKKL <Mkkl3301@gmail.com>
2026-03-06 17:18:53 +0100

    docs: cloud infrastructure docs

--- docs/markdoc.config.mjs
+++ docs/markdoc.config.mjs
@@ -1,6 +1,17 @@
-import { defineMarkdocConfig } from '@astrojs/markdoc/config';
-import starlightMarkdoc from '@astrojs/starlight-markdoc';
+import { defineMarkdocConfig, component } from "@astrojs/markdoc/config";
+import starlightMarkdoc from "@astrojs/starlight-markdoc";
 
 export default defineMarkdocConfig({
-    extends: [starlightMarkdoc()],
+  extends: [starlightMarkdoc()],
+  tags: {
+    ref: {
+      render: component("./src/components/DocLinks.astro"),
+      attributes: {
+        terraform: { type: String },
+        azure: { type: String },
+        repo: { type: String },
+        inline: { type: Boolean },
+      },
+    },
+  },
 });
