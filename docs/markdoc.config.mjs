cbc77d50803277027872f54258771df9cb25b565
MKKL <Mkkl3301@gmail.com>
2026-03-07 15:09:58 +0100

    docs: fix markdoc paths

--- docs/markdoc.config.mjs
+++ docs/markdoc.config.mjs
@@ -3,6 +3,16 @@ import starlightMarkdoc from "@astrojs/starlight-markdoc";
 
 export default defineMarkdocConfig({
   extends: [starlightMarkdoc()],
+  nodes: {
+    image: {
+      render: component("./src/components/MarkdocImage.astro"),
+      attributes: {
+        src: { type: String, required: true },
+        alt: { type: String },
+        title: { type: String },
+      },
+    },
+  },
   tags: {
     ref: {
       render: component("./src/components/DocLinks.astro"),
